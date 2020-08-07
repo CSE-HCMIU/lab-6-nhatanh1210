2e8c995fbfe12fb353913f9f26796b10a562e679
nhatanh1210 <69340268+nhatanh1210@users.noreply.github.com>
2020-08-07 15:53:59 +0700

    Update Exercise4.c

--- Exercise4.c
+++ Exercise4.c
@@ -13,6 +13,39 @@ Ex:
 
 int ex4(char *testcase){
 	//your codes here
+	 int c = 0, d = 0;
+	   char blank[1000];
+	   while (testcase[c] != '\0') {
+      if (testcase[c] == ' ') {
+         int temp = c + 1;
+         if (testcase[temp] != '\0') {
+            while (testcase[temp] == ' ' && testcase[temp] != '\0') {
+               if (testcase[temp] == ' ') {
+                  c++;
+               }  
+               temp++;
+            }
+         }
+      }
+      blank[d] = testcase[c];
+      c++;
+      d++;
+   }
+      blank[d] = '\0';
+ 
+   printf("%s\n", blank);
+ 
+   return 0;
+}
+
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	char *testcase = "  this  is an   unformatted  string ";
+
+	ex4(testcase);
+		
+	return 0;
+}
 }
 
 int main(int argc, char *argv[]) {
