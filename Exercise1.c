6c0bc5c647cc07ca24eb8527784968b1152de766
nhatanh1210 <69340268+nhatanh1210@users.noreply.github.com>
2020-08-07 15:16:33 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -10,7 +10,99 @@
 #include <stdlib.h>
 
 int ex1(int testcase){
-	//your codes here
+	int n,m,i, a[size];
+	printf("Enter number: ");
+	scanf("%d", &n);
+    for (i=0; i<4; i++)
+    {
+    	a[i] = n%10;
+    	n=n/10;
+	}
+    switch (a[3])
+    {
+    	case 1: printf (" one"); 
+    	break;
+		case 2: printf (" two"); 
+    	break;
+    	case 3: printf (" three"); 
+    	break;
+    	case 4: printf (" four"); 
+    	break;
+    	case 5: printf (" five"); 
+    	break;
+    	case 6: printf (" six"); 
+    	break;
+    	case 7: printf (" seven"); 
+    	break;
+    	case 8: printf (" eight"); 
+    	break;
+    	case 9: printf (" nine"); 
+    	break;
+	}
+    printf(" thousand");
+	switch (a[2])
+    {
+    	case 1: printf (" one"); 
+    	break;
+		case 2: printf (" two"); 
+    	break;
+    	case 3: printf (" three"); 
+    	break;
+    	case 4: printf (" four"); 
+    	break;
+    	case 5: printf (" five"); 
+    	break;
+    	case 6: printf (" six"); 
+    	break;
+    	case 7: printf (" seven"); 
+    	break;
+    	case 8: printf (" eight"); 
+    	break;
+    	case 9: printf (" nine"); 
+    	break;
+	}
+	printf (" hundred");
+	switch (a[1])
+    {
+    	case 1: printf (" one"); 
+    	break;
+		case 2: printf (" twenty"); 
+    	break;
+    	case 3: printf (" thirdty"); 
+    	break;
+    	case 4: printf (" forty"); 
+    	break;
+    	case 5: printf (" fifty"); 
+    	break;
+    	case 6: printf (" sixty"); 
+    	break;
+    	case 7: printf (" seventy"); 
+    	break;
+    	case 8: printf (" eighty"); 
+    	break;
+    	case 9: printf (" ninety"); 
+    	break;
+	}
+	switch (a[0])
+    {
+    	case 1: printf (" one"); 
+    	break;
+		case 2: printf (" two"); 
+    	break;
+    	case 3: printf (" three"); 
+    	break;
+    	case 4: printf (" four"); 
+    	break;
+    	case 5: printf (" five"); 
+    	break;
+    	case 6: printf (" six"); 
+    	break;
+    	case 7: printf (" seven"); 
+    	break;
+    	case 8: printf (" eight"); 
+    	break;
+    	case 9: printf (" nine"); 
+    	break;
 }
 
 int main(int argc, char *argv[]) {
